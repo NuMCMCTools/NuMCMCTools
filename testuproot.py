454d8abeba121e551ae416a56b3fc4ec50bb2ad2
Asher <akaboth@Ashers-MacBook-Pro.local>
2024-08-13 15:11:54 +0100

    initial thoughts

---
+++ testuproot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import uproot
+import PlotStack
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+tree = uproot.open("testchaindata.root:mcmc")
+stack = PlotStack.PlotStack(tree)
+
+stack.add_plot(["Deltam2_32", "Theta23"],[],[50,50],[[2.2E-3,2.7E-3],[0.7,0.9]])
+stack.add_plot(["DeltaCP"],[],100,[-3.14159,3.14159])
+
+stack.fill_plots()
+
+stack.plots[0].draw_plot()
+stack.plots[1].draw_plot()
