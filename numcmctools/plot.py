7e47b7dfe91dad0d4714076c67bf6db3ef9f8328
Asher <akaboth@Ashers-MBP.cust.communityfibre.co.uk>
2024-09-03 20:25:48 +0100

    Effort at MO separation. code very rough and not compatable with examples/*py other than mass_ordering.py

--- numcmctools/plot.py
+++ numcmctools/plot.py
@@ -28,10 +28,13 @@ class Plot:
         self.finalized = False
                 
         if(self.nvar==1):
-            self.hist, edges = np.histogram([], self.bins, self.axrange)
+            self.hist, edges = np.histogram([], self.bins, self.axrange, weights=[])
             self.edges.append(edges)
+            if(self.mo_option):
+                self.hist_no = np.zeros(np.shape(self.hist))
+                self.hist_io = np.zeros(np.shape(self.hist))
         elif(self.nvar==2):
-            self.hist, edgesx, edgesy = np.histogram2d([], [],self.bins, self.axrange)
+            self.hist, edgesx, edgesy = np.histogram2d([], [],self.bins, self.axrange, weights=[])
             self.edges.append(edgesx)
             self.edges.append(edgesy)
         else:
@@ -45,10 +48,20 @@ class Plot:
         :data: An array of the data
         :weights: Data weights
         """
+
+        if weights==None:
+            weights = np.ones(np.shape(data[self.variables[0]]))
+        
         if not self.finalized:
             if(self.nvar==1):
-                hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights)
-                self.hist += hist
+                if(self.mo_option):
+                    hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights*np.greater_equal(data["Deltam2_32"],0)*np.ones(np.shape(data["Deltam2_32"])))
+                    self.hist_no += hist
+                    hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights*np.less_equal(data["Deltam2_32"],0)*np.ones(np.shape(data["Deltam2_32"])))
+                    self.hist_io += hist
+                else:
+                    hist, edges = np.histogram(data[self.variables[0]], self.bins, self.axrange, weights = weights)
+                    self.hist += hist
             elif(self.nvar==2):
                 hist, edgesx, edgesy = np.histogram2d(data[self.variables[0]], data[self.variables[1]], self.bins, self.axrange, weights = weights)
                 self.hist += hist
@@ -60,40 +73,77 @@ class Plot:
         Finalize the plot. The plot is finalized to make a probability density function.
         """
         if(not self.finalized):
-            if(self.nvar ==1):
+
+            if(self.nvar==1):
                 self.areas = np.diff(self.edges[0])
+                if(self.mo_option):
+                    self.hist = np.concatenate((self.hist_no,self.hist_io))
+                    self.areas = np.concatenate((self.areas,self.areas))
             if(self.nvar==2):
                 self.areas = np.outer(np.diff(self.edges[0]),np.diff(self.edges[1]))
             total = np.sum(self.hist)
             self.hist = self.hist/self.areas/total
-
+            if self.mo_option:
+                self.hist_no = self.hist[:len(self.hist_no)]
+                self.hist_io = self.hist[len(self.hist_no):]
+            self.finalized = True
             
-    def draw_plot(self, ax):
+    def draw_plot(self, sfig):
         """
         Draw the plot. 
         :ax: matplotlib axes to draw the plot on
         """
+        if self.mo_option:
+            ax = sfig.subplots(1,2, sharey=True)
+        else:
+            ax = sfig.subplots(1,1)
+        
         if(self.nvar==1):
-            ax.stairs(self.hist,self.edges[0])
-            ax.set_xlabel(self.variables[0])
+            if self.mo_option:
+                ax[0].stairs(self.hist_no,self.edges[0])
+                ax[0].set_xlabel(self.variables[0]+" NO")
+                ax[1].stairs(self.hist_io,self.edges[0])
+                ax[1].set_xlabel(self.variables[0]+" IO")
+                                
+            else:
+                ax.stairs(self.hist,self.edges[0])
+                ax.set_xlabel(self.variables[0])
 
         if(self.nvar==2):
             cm = ax.pcolormesh(self.edges[0], self.edges[1], self.hist.T)
             ax.set_xlabel(self.variables[0])
             ax.set_ylabel(self.variables[1])
-            
 
-    def draw_interval(self, ax):
+        sfig.subplots_adjust(wspace=0, hspace=0)
+
+    def draw_interval(self, sfig):
         """
         Draw the intervals. To be improved
         :ax: matplotlib axes to draw the plot on
         """
+
+        if self.mo_option:
+            ax = sfig.subplots(1,2, sharey=True)
+        else:
+            ax = sfig.subplots(1,1)
+        
         #need to put in a check if the intervals have been calculated
         if(self.nvar==1):
-            ax.stairs(self.hist,self.edges[0], color='black')
-            for lev in self.prob_levels:
-                ax.stairs(self.hist*np.greater_equal(self.hist,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
-            ax.set_xlabel(self.variables[0])
+            if self.mo_option:
+                ax[0].stairs(self.hist_no,self.edges[0], color='black')
+                for lev in self.prob_levels:
+                    ax[0].stairs(self.hist_no*np.greater_equal(self.hist_no,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
+                ax[0].set_xlabel(self.variables[0]+" NO")
+                
+                ax[1].stairs(self.hist_io,self.edges[0], color='black')
+                for lev in self.prob_levels:
+                    ax[1].stairs(self.hist_io*np.greater_equal(self.hist_io,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
+                ax[1].set_xlabel(self.variables[0]+" IO")
+            else:
+                ax.stairs(self.hist,self.edges[0], color='black')
+                for lev in self.prob_levels:
+                    ax.stairs(self.hist*np.greater_equal(self.hist,lev),self.edges[0], fill=True, color='grey', alpha=0.3)
+                ax.set_xlabel(self.variables[0])
         
         if(self.nvar==2):
 
@@ -141,8 +191,7 @@ class Plot:
                 index+=1
                 if(process_sum/total > self.levels[nlev]):
                     self.prob_levels[nlev]=self.hist[index_sort_unrav[0][index-1],index_sort_unrav[1][index-1]]
-                    nlev-=1
-            
+                    nlev-=1            
                         
         
         
