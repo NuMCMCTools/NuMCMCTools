c912eace56694274ba5ca2a7844ccef6f9a0cec6
Artur Sztuc <a.sztuc@ucl.ac.uk>
2024-10-07 15:48:45 +0100

    Function to set external lib logging, default "WARN"

--- numcmctools/__init__.py
+++ numcmctools/__init__.py
@@ -7,5 +7,18 @@ from numcmctools.jacobiangraph import JacobianGraph
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s::%(levelname)s::%(name)s: - %(message)s')
 
+def set_external_logging_level(level):
+    """
+    Sets the log level for all the external packages
+
+    :param level: logging level
+    """
+    logging.getLogger("asyncio").setLevel(level)
+    logging.getLogger("fsspec").setLevel(level)
+    logging.getLogger("matplotlib").setLevel(level)
+
 def set_logging_level(level):
     logging.getLogger().setLevel(level)
+
+# Default log level for external packages is "WARNING"
+set_external_logging_level(logging.WARNING)
