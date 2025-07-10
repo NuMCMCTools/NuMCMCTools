da453f83c88068331917b5e0a61f088e1f513738
Artur Sztuc <a.sztuc@ucl.ac.uk>
2025-07-10 12:11:33 +0100

    Extract the constraints from the ROOT file

--- numcmctools/mcmcsamples.py
+++ numcmctools/mcmcsamples.py
@@ -42,6 +42,8 @@ class MCMCSamples:
         self.__check_and_extract_variables()
         # Get self.priors
         self.__check_and_extract_priors()
+        # Get self.constraints
+        self.__check_and_extract_constraints()
         # Get citation
         self.citation = uproot.open(f"{self.filepath}:citation")
 
@@ -69,7 +71,7 @@ class MCMCSamples:
                self.variables[var] = Variable(var, __create_identity_function(var))
 
                # Wrap dcp
-               if var is "DeltaCP":
+               if var == "DeltaCP":
                    self.variables[var].wrap_function(__dcp_wrapper)
 
                logger.debug(f"Compulsory variable {var} found in the root file")
@@ -103,6 +105,69 @@ class MCMCSamples:
         self.variables["JarlskogInvariant"] = Variable("JarlskogInvariant", jarlskog_invariant)
         self.variables["DeltaCP_02pi"] = Variable("DeltaCP_02pi", deltacp_02pi)
         self.variables["DeltaCP_pipi"] = Variable("DeltaCP_pipi", deltacp_pipi)
+    
+    def __parse_constraint_name(self, name: str):
+        """
+        Parse the constraint name to extract the variable names, mass ordering,
+        and whether the default is applied.
+        The expected format is:
+        "unique_name:var1:var2:...:varN:mass_ordering[:default]:applied_default"
+
+        :param name: The name of the constraint.
+        :return: Tuple containing (name, vars, mass_ordering, applied_default).
+        """
+        parts = name.split(":")
+        if len(parts) < 3 or len(parts) > 5:
+            raise ValueError(f"Invalid constraint name format: {name}. Expected at least 3 parts separated by ':'.")
+
+        # Extract the unique name and variables
+        unique_name = parts[0]
+        is_applied_default = int(parts[-1])
+
+        # TODO need extra checks to see how many parts there are, and if user provided e.g. wrong string for MO
+        if parts[-2] in ["NO", "IO"]:
+            mass_ordering = parts[-2]
+            vars = parts[1:-2]
+        else:
+            mass_ordering = "NOIO"
+            vars = parts[1:-1]
+        
+        return unique_name, vars, mass_ordering, is_applied_default
+
+
+    def __check_and_extract_constraints(self):
+        # Open the object with the constraints
+        try:
+            constraints = uproot.open(f"{self.filepath}:constraints")
+        except uproot.exceptions.KeyInFileError:
+            logger.warning("No \"constraints\" TDirectionaryFile found in the ROOT file. Skipping constraints extraction.")
+            return
+
+        logger.warning("\"Constraints\" TDirectionaryFile found in the ROOT file. Reading")
+
+        # Iterate over objects in the constraints TDirectory
+        for obj_name in constraints.keys():
+            # Remove the ;1 if exists
+            obj_name = obj_name[:obj_name.rindex(";")]
+
+            # Parse the constraint name to extract the unique name, variables,
+            # mass ordering, and whether the constraint should be applied by default
+            name, vars, mass_ordering, applied_default = self.__parse_constraint_name(obj_name)
+
+            # Add the variable if it does not exist
+            vars_for_constraint:List[str] = []
+            for var in vars:
+                if var not in self.variables:
+                    func = JacobianGraph.variable_to_func(var, self.compulsory_variables)
+                    self.add_variable(var, func)
+                vars_for_constraint.append(var)
+
+            # Add the constraint
+            self.add_constraint(name,
+                                ExternalConstraint(constraints[obj_name], 
+                                                   vars_for_constraint),
+                                is_inverted=("IO" in mass_ordering),
+                                is_normal=("NO" in mass_ordering))
 
     def add_variable(self, name: str, function: Callable[..., np.ndarray]):
         """
@@ -114,6 +179,8 @@ class MCMCSamples:
                          The function can accept keyqord arguments where keys match the names of
                          already defined variables.
         """
+
+        logger.debug(f"Adding variable '{name}' to MCMCSamples object.")
         if name in self.variables:
            raise ValueError(f"Variable '{name}' is already defined!")
 
@@ -140,6 +207,8 @@ class MCMCSamples:
         if not is_inverted and not is_normal:
             raise ValueError("At least one of is_inverted or is_normal must be True")
 
+        logger.debug(f"Adding constraint '{name}' with {constraint} to MCMCSamples object.")
+            
         self.constraints[name] = constraint
         self.constraints[name].is_inverted = is_inverted
         self.constraints[name].is_normal = is_normal
