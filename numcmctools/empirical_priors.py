e87ac2f5e9a70d918d43d359fe693e0da60882d3
Zoya Vallari <zoya@physics.osu.edu>
2025-09-19 11:56:26 -0400

    fixing interpolation for TGraphs2D; removing regular interpolation for TGraph2D; and hopefully not breaking things for THnT in the process

--- numcmctools/empirical_priors.py
+++ numcmctools/empirical_priors.py
@@ -13,9 +13,9 @@ class EmpiricalPrior:
         type_name = type(root_obj).__name__
         logger.debug(f"Initializing EmpiricalPrior with input root object type: {type_name}")
         if "THnT" in type_name: 
-            self._init_thnd(root_obj)
-        elif "TGraph2D" in type_name:
-            self._init_tgraph2d(root_obj)
+            self._init_thnd(root_obj, interpolator_type)
+        elif "TGraph2D" in type_name: 
+            self._init_tgraph2d(root_obj, interpolator_type)
         else:
             raise ValueError(f"The provided object is not a valid THnT or TGraph2D object. Got {type_name}.")
 
@@ -28,7 +28,7 @@ class EmpiricalPrior:
 
         self.variables = variables
     
-    def _init_thnd(self, thnd):
+    def _init_thnd(self, thnd, interpolator_type):
         # Throw if the number of dimensions is greater than 2
         self.dimensions: int = thnd.member("fNdimensions")
         if self.dimensions > 2:
@@ -41,63 +41,61 @@ class EmpiricalPrior:
         mins = tuple(ax.member("fXmin") for ax in axes)
         maxs = tuple(ax.member("fXmax") for ax in axes)
 
-        # Expand the mins and maxes to take the underflow and overflow bins intothe account, 
+        # Expand the mins and maxes to take the underflow and overflow bins into the account,
         # and move the bins to the center
         step =  tuple((mx - m) / (2*s) for m, mx, s in zip(mins, maxs, shape))
         mins = tuple(m - step[i] for i, m in enumerate(mins))
         maxs = tuple(mx + step[i] for i, mx in enumerate(maxs))
         shape = tuple(s + 2 for s in shape)
 
+        bin_centers = [np.linspace(mn, mx, s) for mn, mx, s in zip(mins, maxs, shape)]
+
         # Load the data from the THnT object
-        self.data: np.ndarray = np.array(thnd.member("fArray").member("fData"), dtype=float)
-        self.data = self.data.reshape(shape)
+        data = np.array(thnd.member("fArray").member("fData"), dtype=float).reshape(shape)
 
-        # Create the bins
-        self.points = np.asarray(tuple(np.linspace(m, mx, s) for m, mx, s in zip(mins, maxs, shape)))
+        if interpolator_type == 'linear':
+            # Cartesian product of bin centers
+            mesh = np.meshgrid(*bin_centers, indexing="ij")
+            points = np.stack([m.flatten() for m in mesh], axis=-1) # (npoints, ndim)
+            data = data.flatten()                                   # (npoints,)
+        elif interpolator_type == 'regular':
+            points = tuple(bin_centers)                             # ndim arrays
+        else:
+            raise ValueError(f"Unknown interpolator_type: {interpolator_type}")
+
+        self.data = data
+        self.points = points
         logger.debug(f"Points shape: {self.points.shape}, z data shape: {self.data.shape}")
-    
-    def _init_tgraph2d(self, tgraph2d):
-        self.dimensions = 2
 
-        x_increasing = not np.all(np.diff(tgraph2d.member('fX')) >= 0)
-        y_increasing = not np.all(np.diff(tgraph2d.member('fY')) >= 0)
+    def _init_tgraph2d(self, tgraph2d, interpolator_type):
+        ''' Initialize from a TGraph2D object with scattered data. 
+            Only 'linear' interpolation is supported.'''
         
-        if x_increasing and y_increasing:
+        self.dimensions = 2
+    
+        x_array = tgraph2d.member('fX')
+        y_array = tgraph2d.member('fY')
+        
+        if not (np.all((np.diff(x_array) >= 0))) and not (np.all((np.diff(y_array) >= 0))):
             raise ValueError("The X and Y axis of the TGraph2D are not increasing.")
 
-        points = []
-        for axis, is_increasing in zip(['fX', 'fY'], [x_increasing, y_increasing]):
-            if is_increasing:
-                npoints = np.where(np.diff(tgraph2d.member(axis)) < 0)[0] + 1
-                points.append(tgraph2d.member(axis)[0:npoints[0]])
-            else:
-                npoints = np.where(np.diff(tgraph2d.member(axis)) > 0)[0] + 1
-                points.append(tgraph2d.member(axis)[::npoints[0]])
-
-        self.data = np.array(tgraph2d.member("fZ"))
-        self.data = self.data.reshape(int(np.sqrt(len(self.data))), int(np.sqrt(len(self.data))))
-        self.points = np.asarray(points)
-
+        if interpolator_type == 'linear':
+            self.data = np.array(tgraph2d.member("fZ"))
+            self.points = np.column_stack((x_array, y_array))
+        elif interpolator_type == 'regular':
+            raise NotImplementedError("'regular' interpolation is not supported for TGraph2D-style scattered data.")
+        else:
+            raise ValueError(f"Unknown interpolator_type: {interpolator_type}")
         logger.debug(f"Points shape: {self.points.shape}, z data shape: {self.data.shape}")
     
     def _init_interpolator(self, interpolator_type: str):
 
         logger.debug(f"Initialising the interpolator of type: {interpolator_type}")
         if interpolator_type == 'linear':
-            # Linear interpolation requires a meshgrid of points for the Delaunay triangulation
-            self.points = np.column_stack([p.ravel() for p in np.meshgrid(*self.points)])
-            self.interpolate = LinearNDInterpolator(self.points,
-                                                    np.ravel(self.data.T),
-                                                    fill_value=0.0,
-                                                    rescale=True)
-
+                self.interpolate = LinearNDInterpolator(self.points, self.data, fill_value=0.0, rescale=True)
         elif interpolator_type == 'regular':
-            self.interpolate = RegularGridInterpolator(self.points,
-                                                       self.data,
-                                                       method='linear',
-                                                       bounds_error=False,
-                                                       fill_value=0.0)
-        
+                self.interpolate = RegularGridInterpolator(self.points, self.data, bounds_error=False, fill_value=0.0)
+
         logger.debug(f"Interpolator initialized: {self.interpolate.__class__.__name__}")
     
     def __call__(self, data):
